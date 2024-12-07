65867503934c3b9f10ffd4552ca6e04735d4c031
V1234-hub <firasviva@hotmail.com>
2024-12-07 14:15:51 +0400

    Add files via upload

---
+++ class15.py
@@ -0,0 +1,56 @@
+
+
+
+
+'''#instance method 
+
+class hat:
+    def __init__(self):
+        self.house= ["gryffindor","styherin","huffepuff", "ravenlaw"]
+
+    #sort the student into house 
+def sorting (self, student):
+    return f"(student)is in {random.choice(self.houses)}"
+'''
+
+
+
+
+
+
+'''import random
+#class method
+
+class Hat:
+
+    houses= {"gryffindor", "styherin", "huffepuff", "ravenlaw"}
+
+    @classmethod
+    def sorting (cls, student):
+        return f"{student}is in {random.choice(cls.houses)}"
+    
+    class student:
+        def __init__(self,name, patrouns):
+            self.name= name 
+            self.patrouns= patrouns
+        
+        def partronus_charm(self):
+            if self. patrouns == "stag":
+                return "𐂂"
+            elif self.patrouns == "enter":
+                return 
+            
+            elif self.patrouns == "jack russel terrier":
+                return "🐶"
+            
+
+    @classmethod 
+    def subjects(cls, subject1, subject2):
+        name= input ("name: ")
+        cls.name= name 
+        return f"{cls.name} is studying {subject1} and {subject2}"
+    '''
+
+    
+
+    
---
+++ class15_test.py
@@ -0,0 +1,51 @@
+
+
+
+
+'''#useing instance method 
+
+def main():
+    try:
+        student= Hat()
+        print(student1.sorting('harry'))
+        student2=hat()
+        print(student2.sorting("hermione"))
+        student3=Hat()
+        print(student3.sorting("ronald"))
+    except Exception as e:
+        print (f"error: {e}")
+'''
+
+
+
+'''
+
+#using class method 
+
+def main():
+    try:
+        print(Hat.sorting("harry"))
+        print(Hat.sorting{"hermione"})
+        print(Hat.sorting("ronald"))
+    except Exception as e:
+        print (f"error: (e)")
+
+if __name__ == "__main__":
+main()
+'''
+
+
+def main():
+    try:
+        name= input("name: ")
+        patronus= input ("patrouns: ")
+        student= student(name.patrouns)
+        print (student.patrouns_charm())
+        print (student.subject)("dark arts,"potions"))
+                                
+    except Exception as e:
+        print (f"error:{e}")
+
+
+if __name__ == "__main__":
+main()
